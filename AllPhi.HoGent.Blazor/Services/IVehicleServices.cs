72d23e79351abd880d2815bb5915a3157b4b6dac
SandrGoethls <sander.goetahls2@student.hogent.be>
2024-04-09 10:56:08 +0200

    Add GetAllVehicleAsync

--- AllPhi.HoGent.Blazor/Services/IVehicleServices.cs
+++ AllPhi.HoGent.Blazor/Services/IVehicleServices.cs
@@ -1,6 +1,11 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+using System.Runtime.InteropServices;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public interface IVehicleServices
     {
+        Task<VehicleListDto> GetAllVehicleAsync([Optional] string? sortBy, [Optional] bool isAscending, [Optional] int pageNumber, [Optional] int pageSize);
+        Task<VehicleDto> GetVehicleByIdAsync(Guid vehicleId);
     }
 }
--- AllPhi.HoGent.Blazor/Services/VehicleServices.cs
+++ AllPhi.HoGent.Blazor/Services/VehicleServices.cs
@@ -1,4 +1,10 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public class VehicleServices : IVehicleServices
     {
@@ -7,6 +13,42 @@
         public VehicleServices(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<VehicleListDto> GetAllVehicleAsync([Optional] string? sortBy, [Optional] bool isAscending, [Optional] int pageNumber, [Optional] int pageSize)
+        {
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            if (!string.IsNullOrEmpty(sortBy)) queryString["sortBy"] = sortBy;
+            queryString["isAscending"] = isAscending.ToString();
+            queryString["pageNumber"] = pageNumber.ToString();
+            queryString["pageSize"] = pageSize.ToString();
+
+            string url = $"api/vehicles/getallvehicles?{queryString}";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error fetching Vehicles: {response.ReasonPhrase}");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var vehiclesDto = JsonConvert.DeserializeObject<VehicleListDto>(responseContent);
+            return vehiclesDto ?? new VehicleListDto();
+        }
+
+        public async Task<VehicleDto> GetVehicleByIdAsync(Guid vehicleId)
+        {
+            var response = await _httpClient.GetAsync($"api/vehicles/getvehiclebyid/{vehicleId}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error fetching fuel card: {response.ReasonPhrase}");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var vehicelDto = JsonConvert.DeserializeObject<VehicleDto>(responseContent);
+            return vehicelDto ?? new();
         }
     }
 }
