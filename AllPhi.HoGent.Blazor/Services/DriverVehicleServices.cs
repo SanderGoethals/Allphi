a7772111fa218c83acb12dd70a56d39472c7da41
SandrGoethls <sander.goetahls2@student.hogent.be>
2024-04-14 18:00:20 +0200

    Add GetDriverWithConnectedVehiclesByDriverId Method

--- AllPhi.HoGent.Blazor/Services/DriverVehicleServices.cs
+++ AllPhi.HoGent.Blazor/Services/DriverVehicleServices.cs
@@ -1,4 +1,8 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public class DriverVehicleServices : IDriverVehicleServices
     {
@@ -9,5 +13,20 @@
             _httpClient = httpClient;
         }
 
+        public async Task<(List<DriverVehicleDto>, bool status, string message)> GetDriverWithConnectedVehiclesByDriverId(Guid driverId)
+        {
+            var response = await _httpClient.GetAsync($"api/drivervehicle/getdriverwithvehiclesbydriverid/{driverId}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error fetching driver's vehicles: {errorResponse}");
+                return (new(), false, $"Error fetching driver's vehicles: {response.ReasonPhrase}");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var driverVehicleDtos = JsonConvert.DeserializeObject<List<DriverVehicleDto>>(responseContent);
+            return (driverVehicleDtos ?? new(), true, "Request successfully");
+        }
     }
 }
--- AllPhi.HoGent.Blazor/Services/IDriverVehicleServices.cs
+++ AllPhi.HoGent.Blazor/Services/IDriverVehicleServices.cs
@@ -1,6 +1,9 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public interface IDriverVehicleServices
     {
+        Task<(List<DriverVehicleDto>, bool status, string message)> GetDriverWithConnectedVehiclesByDriverId(Guid driverId);
     }
 }
